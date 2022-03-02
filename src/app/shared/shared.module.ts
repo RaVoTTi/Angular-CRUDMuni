9a1542552a280bcdff6efbf0821ecb6c1e2cc7b8
RaVoTTi <valenravo@gmail.com>
2022-03-02 00:52:19 -0300

    confusion forms

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppBarComponent } from './app-bar/app-bar.component';
+import { InputsComponent } from './inputs/inputs.component';
 // import { ComponentsComponent } from './components/components.component';
 
 
@@ -9,6 +10,7 @@ import { AppBarComponent } from './app-bar/app-bar.component';
 @NgModule({
   declarations: [
     AppBarComponent,
+    InputsComponent,
 
   ],
   exports:[
